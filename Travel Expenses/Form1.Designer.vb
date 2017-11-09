bc961221de47aa5a01b3f21fe68c4d582354bb99
Ryan Dennick <rdennick@cincinnatistate.edu>
2017-11-09 14:44:54 -0500

    Add project files.

---
+++ Travel Expenses/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDaysOnTrip = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtHotelCost = New System.Windows.Forms.TextBox()
+        Me.txtAirfare = New System.Windows.Forms.TextBox()
+        Me.txtParkingAmount = New System.Windows.Forms.TextBox()
+        Me.txtMealAmount = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblReimbursementTotal = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Number of Days on Trip"
+        '
+        'txtDaysOnTrip
+        '
+        Me.txtDaysOnTrip.Location = New System.Drawing.Point(156, 30)
+        Me.txtDaysOnTrip.Name = "txtDaysOnTrip"
+        Me.txtDaysOnTrip.Size = New System.Drawing.Size(100, 20)
+        Me.txtDaysOnTrip.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Total Cost of Hotel"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(71, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Total Airfare"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 175)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Total Amount of Parking"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 219)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(116, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Total Amount for Meals"
+        '
+        'txtHotelCost
+        '
+        Me.txtHotelCost.Location = New System.Drawing.Point(156, 76)
+        Me.txtHotelCost.Name = "txtHotelCost"
+        Me.txtHotelCost.Size = New System.Drawing.Size(100, 20)
+        Me.txtHotelCost.TabIndex = 6
+        '
+        'txtAirfare
+        '
+        Me.txtAirfare.Location = New System.Drawing.Point(156, 124)
+        Me.txtAirfare.Name = "txtAirfare"
+        Me.txtAirfare.Size = New System.Drawing.Size(100, 20)
+        Me.txtAirfare.TabIndex = 7
+        '
+        'txtParkingAmount
+        '
+        Me.txtParkingAmount.Location = New System.Drawing.Point(156, 168)
+        Me.txtParkingAmount.Name = "txtParkingAmount"
+        Me.txtParkingAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtParkingAmount.TabIndex = 8
+        '
+        'txtMealAmount
+        '
+        Me.txtMealAmount.Location = New System.Drawing.Point(156, 212)
+        Me.txtMealAmount.Name = "txtMealAmount"
+        Me.txtMealAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtMealAmount.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 264)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(107, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Total Reimbursement"
+        '
+        'lblReimbursementTotal
+        '
+        Me.lblReimbursementTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblReimbursementTotal.Location = New System.Drawing.Point(156, 254)
+        Me.lblReimbursementTotal.Name = "lblReimbursementTotal"
+        Me.lblReimbursementTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblReimbursementTotal.TabIndex = 11
+        Me.lblReimbursementTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(29, 303)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 12
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(128, 303)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 13
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(226, 303)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 14
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(331, 352)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblReimbursementTotal)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtMealAmount)
+        Me.Controls.Add(Me.txtParkingAmount)
+        Me.Controls.Add(Me.txtAirfare)
+        Me.Controls.Add(Me.txtHotelCost)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDaysOnTrip)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Travel Expenses"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDaysOnTrip As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtHotelCost As TextBox
+    Friend WithEvents txtAirfare As TextBox
+    Friend WithEvents txtParkingAmount As TextBox
+    Friend WithEvents txtMealAmount As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblReimbursementTotal As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Travel Expenses/Form1.vb
@@ -0,0 +1,294 @@
+﻿'*****************************************************************
+'Ryan Dennick
+'11/01/2017
+'Travel Expenses
+'Travel expenses takes in five inputs: number of days on the trip
+'hotel per day cost, airfare, parking amount from the trip and
+'meal amount for the trip.  Travel expenses has one output: total
+'reimbursement for the trip. Reimbursements are limited to 95 per
+'day on hotels, 10 per day on parking and 37 per day for meals.
+'If inputs are less than reimbursement caps, employee will be 
+'reimbursed the amount entered times the amount of days on the trip
+'*****************************************************************
+
+Option Strict On
+
+Public Class Form1
+
+
+    Structure Numbers
+        Dim intDaysOnTrip As Integer
+        Dim dblHotelCost As Double
+        Dim dblAirfare As Double
+        Dim dblParkingAmount As Double
+        Dim dblMealAmount As Double
+    End Structure
+
+    Dim blnOK As Boolean = True
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        'Create local variables
+        Dim Number As Numbers
+        Dim dblTotalReimbursement As Double
+
+        txtDaysOnTrip.BackColor = Color.White
+        txtHotelCost.BackColor = Color.White
+        txtAirfare.BackColor = Color.White
+        txtParkingAmount.BackColor = Color.White
+        txtMealAmount.BackColor = Color.White
+
+        Try
+            'Validate inputs
+            Validation(Number)
+
+            'Check validations
+            If blnOK = True Then
+
+                'Calculate total reimbursement for trip
+                dblTotalReimbursement = CalcLodging(Number) + CalcParkingFees(Number) + CalcMeals(Number)
+                'Display output
+                Display(dblTotalReimbursement.ToString("c"))
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+    Private Function CalcLodging(ByVal Nums As Numbers) As Double
+
+        'Create function to calculate hotel reimbursement
+
+        'Create per day cost and hotel reimbursement variables
+        Dim dblPerDayCost As Double
+        Dim dblHotelReimbursement As Double
+        'Create hotel reimbursement constant
+        Const intHotelReimbursementDailyAmount As Integer = 95
+
+        dblPerDayCost = Nums.dblHotelCost / Nums.intDaysOnTrip
+
+        If dblPerDayCost <= intHotelReimbursementDailyAmount Then
+            dblHotelReimbursement = dblPerDayCost * Nums.intDaysOnTrip
+        Else
+            dblHotelReimbursement = intHotelReimbursementDailyAmount * Nums.intDaysOnTrip
+        End If
+
+        Return dblHotelReimbursement
+
+    End Function
+
+    Private Function CalcParkingFees(ByVal Nums As Numbers) As Double
+
+        'Create function to calculate parking reimbursement
+
+        'Create per day cost and parking reimbursement variables
+        Dim dblPerDayCost As Double
+        Dim dblParkingReimbursement As Double
+        'Create parking reimbursement constant
+        Const intParkingReimbursementDailyAmount As Integer = 10
+
+        dblPerDayCost = Nums.dblParkingAmount / Nums.intDaysOnTrip
+
+        If dblPerDayCost <= intParkingReimbursementDailyAmount Then
+            dblParkingReimbursement = dblPerDayCost * Nums.intDaysOnTrip
+        Else
+            dblParkingReimbursement = intParkingReimbursementDailyAmount * Nums.intDaysOnTrip
+        End If
+
+        Return dblParkingReimbursement
+
+    End Function
+
+
+    Private Function CalcMeals(ByVal Nums As Numbers) As Double
+
+        'Create function to calculate meal reimbursement
+
+        'Create per day cost and meal reimbursement variables
+        Dim dblPerDayCost As Double
+        Dim dblMealReimbursement As Double
+        'Create meal reimbursement constant
+        Const dblMealReimbursementDailyAmount As Double = 37
+
+        dblPerDayCost = Nums.dblMealAmount / Nums.intDaysOnTrip
+
+        If dblPerDayCost <= dblMealReimbursementDailyAmount Then
+            dblMealReimbursement = dblPerDayCost * Nums.intDaysOnTrip
+        Else
+            dblMealReimbursement = dblMealReimbursementDailyAmount * Nums.intDaysOnTrip
+        End If
+
+        Return dblMealReimbursement
+
+
+    End Function
+
+    Private Sub Display(ByVal Output As String)
+
+        'Display the output for the application
+        lblReimbursementTotal.Text = Output
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clear input and output controls
+        ResetDaysOnTrip()
+        ResetHotelCost()
+        ResetAirfare()
+        ResetParkingAmount()
+        ResetMealAmount()
+        ResetReimbursementAmount()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Exits application
+        Me.Close()
+
+    End Sub
+
+    Private Sub Validation(ByRef Number As Numbers)
+
+        'Validate inputs
+        If IsNumeric(txtDaysOnTrip.Text) Then
+            Number.intDaysOnTrip = CInt(txtDaysOnTrip.Text)
+        Else
+            MessageBox.Show("Please enter numeric values only for number of days on trip")
+            txtDaysOnTrip.Focus()
+            txtDaysOnTrip.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If Number.intDaysOnTrip < 1 Then
+            MessageBox.Show("Please enter at least one for days on trip")
+            txtDaysOnTrip.Focus()
+            txtDaysOnTrip.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If IsNumeric(txtHotelCost.Text) Then
+            Number.dblHotelCost = CDbl(txtHotelCost.Text)
+        Else
+            MessageBox.Show("Please enter numeric values only for hotel cost")
+            txtHotelCost.Focus()
+            txtHotelCost.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If Number.dblHotelCost < 0 Then
+            MessageBox.Show("Please enter a value greater than zero for hotel cost")
+            txtHotelCost.Focus()
+            txtHotelCost.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If IsNumeric(txtAirfare.Text) Then
+            Number.dblAirfare = CDbl(txtAirfare.Text)
+        Else
+            MessageBox.Show("Please enter numeric values only for hotel cost")
+            txtAirfare.Focus()
+            txtAirfare.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If Number.dblAirfare < 0 Then
+            MessageBox.Show("Please enter a value greater than zero for airfare cost")
+            txtAirfare.Focus()
+            txtAirfare.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If IsNumeric(txtParkingAmount.Text) Then
+            Number.dblParkingAmount = CDbl(txtParkingAmount.Text)
+        Else
+            MessageBox.Show("Please enter numeric values only for parking cost")
+            txtParkingAmount.Focus()
+            txtParkingAmount.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If Number.dblParkingAmount < 0 Then
+            MessageBox.Show("Please enter a value greater than zero for parking cost")
+            txtParkingAmount.Focus()
+            txtParkingAmount.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If IsNumeric(txtMealAmount.Text) Then
+            Number.dblMealAmount = CDbl(txtMealAmount.Text)
+        Else
+            MessageBox.Show("Please enter numeric values only for meal cost")
+            txtMealAmount.Focus()
+            txtMealAmount.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        If Number.dblMealAmount < 0 Then
+            MessageBox.Show("Please enter a value greater than zero for meal cost")
+            txtMealAmount.Focus()
+            txtMealAmount.BackColor = Color.Yellow
+            blnOK = False
+            Exit Sub
+        End If
+
+        blnOK = True
+
+    End Sub
+
+    Private Sub ResetDaysOnTrip()
+
+        'Reset days spent on trip
+        txtDaysOnTrip.Text = String.Empty
+
+    End Sub
+
+    Private Sub ResetHotelCost()
+
+        'Reset hotel cost per day
+        txtHotelCost.Text = String.Empty
+
+    End Sub
+
+    Private Sub ResetAirfare()
+
+        'Reset airfare cost
+        txtAirfare.Text = String.Empty
+
+    End Sub
+
+    Private Sub ResetParkingAmount()
+
+        'Reset parking amount
+        txtParkingAmount.Text = String.Empty
+
+    End Sub
+
+    Private Sub ResetMealAmount()
+
+        'Reset meal amount
+        txtMealAmount.Text = String.Empty
+
+    End Sub
+
+    Private Sub ResetReimbursementAmount()
+
+        'Reset reimbursement label
+        lblReimbursementTotal.Text = String.Empty
+
+    End Sub
+End Class
